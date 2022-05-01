d5997b7b67bedd7cfa4f5766c43ab3f1e78bc46d
nullxDEADBEEF <andxdgu@gmail.com>
2022-05-01 17:02:32 +0200

    Add message transformation to and from JSON,XML and YAML

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,14 +16,13 @@ import (
 // - Consumer informs ESB to get messages from a specific provider
 
 type User struct {
-	Id    string `json:"id" xml:"Id"`
-	Token string `json:"token" xml:"Token"`
+	Id    string `json:"id" xml:"id"`
+	Token string `json:"token" xml:"token"`
 }
 
-type ProviderMessage struct {
-	Id        string `json:"id" xml:"Id"`
-	Message   string `json:"message" xml:"Message"`
-	CreatedAt string `json:"created_at" xml:"CreatedAt"`
+type Message struct {
+	Id      string `form:"id" json:"id" xml:"id" yaml:"id"`
+	Content string `form:"content" json:"content" xml:"content" yaml:"content"`
 }
 
 var users = []User{
@@ -35,49 +36,78 @@ var users = []User{
 	},
 }
 
-var messages = map[string][]ProviderMessage{
+var messages = map[string][]Message{
 	"1": {
 		{
-			Id: "1a76658a-7e4c-4f24-9a96-f68ef3526008", Message: "I am message 1", CreatedAt: "1650807757",
+			Id: "1a76658a-7e4c-4f24-9a96-f68ef3526008", Content: "I am message 1",
 		},
 		{
 
-			Id: "87a17661-13ae-45c3-bfb2-041afa15234a", Message: "I am message 2", CreatedAt: "1650807758",
+			Id: "87a17661-13ae-45c3-bfb2-041afa15234a", Content: "I am message 2",
 		},
 		{
-			Id: "aa229257-48f3-46e2-88dc-beb210e7f9e4", Message: "I am message 3", CreatedAt: "1650807759",
+			Id: "aa229257-48f3-46e2-88dc-beb210e7f9e4", Content: "I am message 3",
 		},
 		{
-			Id: "410cc421-71c0-414a-99d6-6e603e741692", Message: "I am message 4", CreatedAt: "1650807760",
+			Id: "410cc421-71c0-414a-99d6-6e603e741692", Content: "I am message 4",
 		},
 	},
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/provider/:id/token/:token", esb)
+	router.POST("/create-message", messageHandler)
+	router.GET("/provider/:id/limit/:limit/token/:token", esb)
 	router.Run(":9999")
 }
 
+func messageHandler(c *gin.Context) {
+	token := c.Query("token")
+	format := c.Query("format")
+	message := Message{}
+	c.Bind(&message)
+	for _, user := range users {
+		if token == user.Token {
+			transformMessage(c, message, format)
+			return
+		}
+	}
+	c.JSON(http.StatusForbidden, gin.H{"detail": "Invalid token"})
+}
+
 func esb(c *gin.Context) {
-	token := c.Param("token")
-	msg_id := c.Param("id")
+	consumerToken := c.Param("token")
+	//msgId := c.Param("id")
+	msgLimit, err := strconv.Atoi(c.Param("limit"))
+	if err != nil {
+		log.Fatal("Could not convert limit to int")
+	}
+
+	if msgLimit <= 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"info": "Limit is 0 or less"})
+		return
+	}
 	for _, v := range users {
-		if v.Token == token {
+		if v.Token == consumerToken {
 			// transform message based on consumer
-			transformMessage(c, token, messages[msg_id])
+			// TODO: handle properly
+			//transformMessage(c, consumerToken, message)
 			return
 		}
 	}
 
-	c.JSON(http.StatusUnauthorized, gin.H{"info": "Invalid token"})
+	c.JSON(http.StatusForbidden, gin.H{"info": "Invalid token"})
 }
 
-func transformMessage(c *gin.Context, consumerToken string, message []ProviderMessage) {
-	if consumerToken == "1212" {
+func transformMessage(c *gin.Context, message Message, format string) {
+	switch format {
+	case "JSON":
 		JSONTransformer(c, message)
-		return
-	} else {
+	case "XML":
 		XMLTransformer(c, message)
+	case "YAML":
+		YAMLTransformer(c, message)
+	default:
+		c.JSON(http.StatusBadRequest, gin.H{"detail": "Invalid format"})
 	}
 }
--- transformer.go
+++ transformer.go
@@ -6,12 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func JSONTransformer(c *gin.Context, message []ProviderMessage) {
+func JSONTransformer(c *gin.Context, message Message) {
 	c.JSON(http.StatusOK, message)
 }
 
-func XMLTransformer(c *gin.Context, message []ProviderMessage) {
+func XMLTransformer(c *gin.Context, message Message) {
 	c.XML(http.StatusOK, message)
+}
+
+func YAMLTransformer(c *gin.Context, message Message) {
+	c.YAML(http.StatusOK, message)
 }
 
 func TSVTransformer() {
